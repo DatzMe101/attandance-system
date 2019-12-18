febc624ff255e486f3443ebe6840636d0b7191ed
Elmer Datolayta <elmer.datolayta@devpartners.co>
2019-12-18 15:48:54 +0800

    add cors on routes

--- startup/routes.js
+++ startup/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
 const students = require('../routes/students');
 
 module.exports = app => {
   app.use(express.json());
+  app.use(cors({ origin: '*' }));
   app.use('/api/register', users);
   app.use('/api/login', auth);
   app.use('/api/students', students);
